632fb04e562a07d24ffc8921bd5effd4b7dd17fa
Tom Mornini <tom@subledger.com>
2019-10-03 13:24:05 -0700

    ./test checks out specs.output

--- main.go
+++ main.go
@@ -2,19 +2,76 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	for scanner.Scan() {
-		line := scanner.Text()
+	power := 10
 
-		println(line)
+	for scanner.Scan() {
+		value, err := strconv.Atoi(scanner.Text())
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(
+			handlePower(power, value),
+		)
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
+	}
+}
+
+func handlePower(power int, value int) string {
+	switch value {
+	case 0:
+		return "zero"
+	case 1:
+		return "one"
+	case 2:
+		return "two"
+	case 3:
+		return "three"
+	case 4:
+		return "four"
+	case 5:
+		return "five"
+	case 6:
+		return "six"
+	case 7:
+		return "seven"
+	case 8:
+		return "eight"
+	case 9:
+		return "nine"
+	case 10:
+		return "ten"
+	case 11:
+		return "eleven"
+	case 12:
+		return "twelve"
+	case 13:
+		return "thirteen"
+	case 14:
+		return "fourteen"
+	case 15:
+		return "fifteen"
+	case 16:
+		return "sixteen"
+	case 17:
+		return "seventeen"
+	case 18:
+		return "eighteen"
+	case 19:
+		return "nineteen"
+	default:
+		return "?"
 	}
 }

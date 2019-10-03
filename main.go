5f7d98c805b814443e09f9e0a7f04f9b43d072ee
Tom Mornini <tom@subledger.com>
2019-10-03 13:22:33 -0700

    add basic input handling

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		println(line)
+	}
+
+	if scanner.Err() != nil {
+		panic(scanner.Err())
+	}
 }
